34acc41b6f377a5bd74416d9b62c263e771d4d80
Gianluca Regis <gianluca.regis@mail.polimi.it>
2020-05-09 19:21:13 +0200

    Implemented User bean

--- src/it/polimi/tiw/crowdsourcing/beans/User.java
+++ src/it/polimi/tiw/crowdsourcing/beans/User.java
@@ -10,6 +10,61 @@ public class User {
     private String password;
     private String email;
     private ExperienceLevel experienceLevel;
+    private String picture;
 
+    public int getId() {
+        return id;
+    }
 
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public ExperienceLevel getExperienceLevel() {
+        return experienceLevel;
+    }
+
+    public void setExperienceLevel(ExperienceLevel experienceLevel) {
+        this.experienceLevel = experienceLevel;
+    }
+
+    public String getPicture() {
+        return picture;
+    }
+
+    public void setPicture(String picture) {
+        this.picture = picture;
+    }
 }
