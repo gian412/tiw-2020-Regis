cb686b136daa5fcab13b013be4bded21fb0d6504
Gianluca Regis <gianluca.regis@mail.polimi.it>
2020-05-09 19:52:44 +0200

    Implemented CampaignStatus enum

--- src/it/polimi/tiw/crowdsourcing/utils/CampaignStatus.java
+++ src/it/polimi/tiw/crowdsourcing/utils/CampaignStatus.java
@@ -1,4 +1,26 @@
 package it.polimi.tiw.crowdsourcing.utils;
 
 public enum CampaignStatus {
+
+    CREATED(0), STARTED(1), CLOSED(2);
+
+    private final int value;
+
+    CampaignStatus(int value) {
+        this.value = value;
+    }
+
+    public static CampaignStatus getCampaignStatusFromInt(int value) {
+        return switch (value) {
+            case 0 -> CampaignStatus.CREATED;
+            case 1 -> CampaignStatus.STARTED;
+            case 2 -> CampaignStatus.CLOSED;
+            default -> null;
+        };
+    }
+
+    public int getValue() {
+        return this.value;
+    }
+
 }
