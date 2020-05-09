994d1b3da86ab2a4f6772b82f15cf633fc8f85c6
Gianluca Regis <gianluca.regis@mail.polimi.it>
2020-05-09 19:28:28 +0200

    Created and implemented Campaign bean and CampaignStatus

---
+++ src/it/polimi/tiw/crowdsourcing/beans/Campaign.java
@@ -0,0 +1,52 @@
+package it.polimi.tiw.crowdsourcing.beans;
+
+import it.polimi.tiw.crowdsourcing.utils.CampaignStatus;
+
+public class Campaign {
+
+    private int id;
+    private String name;
+    private String customer;
+    private CampaignStatus status;
+    private int managerId;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCustomer() {
+        return customer;
+    }
+
+    public void setCustomer(String customer) {
+        this.customer = customer;
+    }
+
+    public CampaignStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(CampaignStatus status) {
+        this.status = status;
+    }
+
+    public int getManagerId() {
+        return managerId;
+    }
+
+    public void setManagerId(int managerId) {
+        this.managerId = managerId;
+    }
+}
