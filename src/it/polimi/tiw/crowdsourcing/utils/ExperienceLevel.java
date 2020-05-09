f10a1b8e9bc9acb12ed841cb43be22dfe1c8400b
Gianluca Regis <gianluca.regis@mail.polimi.it>
2020-05-09 19:47:28 +0200

    Implemented ExperienceLevel enum

--- src/it/polimi/tiw/crowdsourcing/utils/ExperienceLevel.java
+++ src/it/polimi/tiw/crowdsourcing/utils/ExperienceLevel.java
@@ -1,4 +1,26 @@
 package it.polimi.tiw.crowdsourcing.utils;
 
 public enum ExperienceLevel {
+
+    ALTO(0), MEDIO(1), BASSO(2);
+
+    private final int value;
+
+    ExperienceLevel(int value) {
+        this.value = value;
+    }
+
+    public static ExperienceLevel getExperienceLevelFromInt(int value) {
+        return switch (value) {
+            case 0 -> ExperienceLevel.ALTO;
+            case 1 -> ExperienceLevel.MEDIO;
+            case 2 -> ExperienceLevel.BASSO;
+            default -> null;
+        };
+    }
+
+    public int getValue() {
+        return this.value;
+    }
+
 }
