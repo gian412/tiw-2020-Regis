26b82b4fddb7ea9224494f76fa6004762ea2ff8b
Gianluca Regis <gianluca.regis@mail.polimi.it>
2020-05-09 20:00:04 +0200

    Implemented Resolution enum and Point

--- src/it/polimi/tiw/crowdsourcing/utils/Point.java
+++ src/it/polimi/tiw/crowdsourcing/utils/Point.java
@@ -1,4 +1,21 @@
 package it.polimi.tiw.crowdsourcing.utils;
 
 public class Point {
+
+    private final double latitude;
+    private final double longitude;
+
+    public Point(double latitude, double longitude) {
+        this.latitude = latitude;
+        this.longitude = longitude;
+    }
+
+    public double getLatitude() {
+        return latitude;
+    }
+
+    public double getLongitude() {
+        return longitude;
+    }
+
 }
--- src/it/polimi/tiw/crowdsourcing/utils/Resolution.java
+++ src/it/polimi/tiw/crowdsourcing/utils/Resolution.java
@@ -1,4 +1,26 @@
 package it.polimi.tiw.crowdsourcing.utils;
 
 public enum  Resolution {
+
+    HIGH(0), MEDIUM(1), LOW(2);
+
+    private final int value;
+
+    Resolution(int value) {
+        this.value = value;
+    }
+
+    public static Resolution getResolutionFromInt(int value) {
+        return switch (value) {
+            case 0 -> Resolution.HIGH;
+            case 1 -> Resolution.MEDIUM;
+            case 2 -> Resolution.LOW;
+            default -> null;
+        };
+    }
+
+    public int getValue() {
+        return this.value;
+    }
+
 }
