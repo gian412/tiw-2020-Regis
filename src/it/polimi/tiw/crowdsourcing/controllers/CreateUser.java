41f815ac3ea9cdc2ecf06c32e4996e646d897298
Gianluca Regis <gianluca.regis@mail.polimi.it>
2020-05-11 23:05:57 +0200

    Changed Login/SignUp method

---
+++ src/it/polimi/tiw/crowdsourcing/controllers/CreateUser.java
@@ -0,0 +1,9 @@
+package it.polimi.tiw.crowdsourcing.controllers;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+
+@WebServlet("/CreateUser")
+public class CreateUser extends HttpServlet {
+    // TODO: complete
+}
