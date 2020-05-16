ffb873f654e6c5254737630b53444d5cbd918d1f
Gianluca Regis <gianluca.regis@mail.polimi.it>
2020-05-16 03:12:25 +0200

    Created GoToCampaignDetails

---
+++ src/it/polimi/tiw/crowdsourcing/controllers/GoToCampaignDetails.java
@@ -0,0 +1,51 @@
+package it.polimi.tiw.crowdsourcing.controllers;
+
+import it.polimi.tiw.crowdsourcing.utils.ClientHandler;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.templatemode.TemplateMode;
+import org.thymeleaf.templateresolver.ServletContextTemplateResolver;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+@WebServlet("/CampaignDetails")
+public class GoToCampaignDetails extends HttpServlet {
+
+    private static final long serialVersionUID = 1L;
+    private Connection connection;
+    private TemplateEngine templateEngine;
+
+    @Override
+    public void init() throws ServletException {
+        connection = ClientHandler.getConnection(getServletContext());
+        ServletContext servletContext = getServletContext();
+        ServletContextTemplateResolver templateResolver = new ServletContextTemplateResolver( servletContext );
+        templateResolver.setTemplateMode(TemplateMode.HTML);
+        this.templateEngine = new TemplateEngine();
+        this.templateEngine.setTemplateResolver(templateResolver);
+        templateResolver.setSuffix(".html");
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        System.out.println("Hello world!");
+
+    }
+
+    @Override
+    public void destroy() {
+        try {
+            ClientHandler.closeConnection(connection);
+        } catch (SQLException e) {
+            e.printStackTrace(); // TODO: remove after test
+        }
+    }
+}
