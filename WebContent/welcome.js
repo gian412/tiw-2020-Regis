cd3cb5b55a5f0480f254f90ae3a85b5fadb1da91
Gianluca Regis <gianluca.regis@mail.polimi.it>
2020-06-01 01:45:40 +0200

    Making login and signUp operation more interactive

---
+++ WebContent/welcome.js
@@ -0,0 +1,10 @@
+(function () {
+
+  document.getElementById('login').addEventListener('click', (e)=>{
+
+  let form = document.getElementById('loginForm');
+  form.style.display='block';
+
+  });
+
+})();
